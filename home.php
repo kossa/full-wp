a41201e6a22fa2a2e723bda525f4ac4c0956715f
kossa <hadjikouceyla@gmail.com>
2015-06-06 18:00:55 +0100

    Make home into page

--- home.php
+++ home.php
@@ -1,5 +1,7 @@
 <?php 
-
+/**
+* Template Name: Home Page
+*/
   get_header();
 
 ?>
@@ -11,75 +13,13 @@
       </div>
     </section><!-- end banner -->
 
-    <section class="what-we-do">
-      <div class="container">
-        <h2><span>What we do</span></h2>
-        <div class="row">
-          <div class="col-sm-6">
-            <div class="row">
-              <div class="col-sm-3 text-center">
-                <img src="<?php echo bloginfo( 'url' ); ?>/wp-content/uploads/2015/05/img-01.png" alt="">
-              </div>
-              <div class="col-sm-9">
-                <h3>Response Management</h3>
-                <p>Drive revenue with complete visibility into your critical marketing data.</p>
-                <a href="#" class="btn btn-primary">Learn more ›</a>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm-6">
-            <div class="row">
-              <div class="col-sm-3 text-center">
-                <img src="<?php echo bloginfo( 'url' ); ?>/wp-content/uploads/2015/05/img-02.png" alt="">
-              </div>
-              <div class="col-sm-9">
-                <h3>Campaign Influence</h3>
-                <p>The campaign performance metrics you need to plan with confidence.</p>
-                <a href="#" class="btn btn-primary">Learn more ›</a>
-              </div>
-            </div>
-          </div>
-
-        </div>
-      </div>
-      <div class="leading-marketing">
-        <div class="container">
-          <h3>But that’s not all.</h3>
-          <p>Learn more about the leading Marketing Performance Management applications on the Salesforce1 Platform.</p>
-          <div class="text-center"><a href="#" class="btn btn-primary">Check out our full product family ›</a></div>
-        </div>
-      </div>
-    </section><!-- end what-we-do -->
-
-    <section class="learn-marketing-data">
-      <div class="container">
-        <h2><span>Learn From the Marketing Data Experts</span></h2>
-
-        <div class="row">
-          <div class="col-sm-4">
-            <div class="one-section">
-              <h5><i class="fa fa-book"></i> <br>White paper: </h5>
-              <h4>Data-Driven Marketing is Here to Stay—Campaign influence for Salesforce Marketers</h4>
-              <a href="#" class="btn btn-primary">download now ›</a>
-            </div>
-          </div>
-          <div class="col-sm-4">
-            <div class="one-section">
-              <h5><i class="fa fa-play-circle"></i> <br>Webinar: </h5>
-              <h4>Align Sales and Marketing with Funnel Metrics in Salesforce </h4>
-              <a href="#" class="btn btn-primary">watch now ›</a>
-            </div>
-          </div>
-          <div class="col-sm-4">
-            <div class="one-section">
-              <h5><i class="fa fa-book"></i> <br>White paper: </h5>
-              <h4>Best Practices in Designing a Lead Lifecycle in Salesforce</h4>
-              <a href="#" class="btn btn-primary">get it now ›</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section><!-- learn-marketing-data -->
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <?php the_content(); ?>
+    <?php endwhile; ?>
+    <!-- post navigation -->
+    <?php else: ?>
+    <!-- no posts found -->
+    <?php endif; ?>
 
     <?php load_template( dirname(__FILE__) . '/inc/sales-force.php'); ?>
 
--- page.php
+++ page.php
@@ -1,9 +1,30 @@
 <?php get_header(); ?>
 
 
+
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-    <?php the_content( ); ?>
+    <?php echo getField('banner'); ?>
+
+        <div id="sources-page">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-7">
+
+                <?php the_content(); ?>
+
+              </div>
+
+              <div class="col-sm-5">
+                <section class="sign-up">
+                  <h2>Sign up now!</h2>
+                  <p><em>Please complete all fields</em></p>
+                  <?php echo do_shortcode( '[contact-form-7 id="276" title="Sign up now"]' ); ?>
+                </section>
+              </div>
+            </div>
+          </div>
+        </div>
 
 <?php endwhile; ?>
 <!-- post navigation -->
