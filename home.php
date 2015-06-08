f4c944eeaf0aad47d44e6998a6b6f6af9ba90462
kossa <hadjikouceyla@gmail.com>
2015-06-08 17:59:07 +0100

    Home - make banner dynamic

--- home.php
+++ home.php
@@ -5,15 +5,10 @@
   get_header();
 
 ?>
-    <section class="banner">
-      <div class="container">
-        <h1>Bulletproof your marketing data</h1>
-        <h3>Rock solid campaign performance metrics <br> everyone can agree on – right in Salesforce.</h3>
-        <a href="#" class="btn btn-warning">watch a demo ›</a>
-      </div>
-    </section><!-- end banner -->
 
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <?php echo do_shortcode(getField('banner')); ?>
+      
       <?php the_content(); ?>
     <?php endwhile; ?>
     <!-- post navigation -->
