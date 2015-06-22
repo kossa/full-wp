32b45542e640fb3151cb00a636b8250bd863aeac
kossa <hadjikouceyla@gmail.com>
2015-06-22 19:21:00 +0100

    Add Quote editor on interior pages Fixes #31

---
+++ inc/static-quote.php
@@ -0,0 +1,20 @@
+
+
+<section class="data-driven">
+  <div class="container">
+    <div class="text-center">
+      <h2><span>What people are saying</span></h2>
+    </div>
+
+    <div id="foo1">
+
+        <div class="row">
+          <div class="col-lg-8 col-md-offset-2">
+            <?php echo getField('quotes') ?>
+            <div class="clearfix"></div>
+          </div>
+        </div>
+    </div>
+
+  </div>
+</section>
