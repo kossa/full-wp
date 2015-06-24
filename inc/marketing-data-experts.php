427ef0b0a9a3948843d430b105416fa45d3dd18d
Polito Technologies <info@politotech.com>
2015-06-24 11:08:21 -0400

    Update marketing-data-experts.php

--- inc/marketing-data-experts.php
+++ inc/marketing-data-experts.php
@@ -5,17 +5,17 @@
           <div class="col-sm-6">
             <article>
               <h4><i class="fa fa-rss"></i> Blog Posts: </h4>
-              <p>Best Practices in Designing a Lead Lifecycle in Salesforce</p>
-              <p>Turbo Charge Your Analytics in Salesforce</p>
-              <p>Optimize Your Marketing Mix in Salesforce</p>
+              <a href="http://104.236.242.180/full-wp/how-to-get-sales-and-marketing-in-alignment/"><p>How To Get Sales And Marketing in Alignment</p></a>
+              <a href="http://104.236.242.180/full-wp/why-executives-need-data-driven-insights-from-marketing/"><p>Why Executives Need Data-Driven Insights From Marketing</p></a>
+              <a href="http://104.236.242.180/full-wp/how-to-get-key-marketing-metrics-from-your-crm-system/"><p>How To Get Key Marketing Metrics From Your CRM System</p></a>
             </article>
           </div>
           <div class="col-sm-6">
             <article>
               <h4><i class="fa fa-book"></i> Resources: </h4>
-              <p>Best Practices in Designing a Lead Lifecycle in Salesforce</p>
-              <p>Turbo Charge Your Analytics in Salesforce</p>
-              <p>Optimize Your Marketing Mix in Salesforce</p>
+              <a href="http://104.236.242.180/full-wp/best-practices-for-designing-a-lead-lifecycle-in-salesforce/"><p>Best Practices in Designing a Lead Lifecycle in Salesforce</p></a>
+              <a href="http://104.236.242.180/full-wp/turbo-charge-your-analytics-in-salesforce-2/"><p>Turbo Charge Your Analytics in Salesforce</p></a>
+              <a href="http://104.236.242.180/full-wp/optimize-your-marketing-mix-in-salesforce/"><p>Optimize Your Marketing Mix in Salesforce</p></a>
             </article>
           </div>
         </div>
