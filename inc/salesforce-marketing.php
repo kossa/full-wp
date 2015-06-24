078bdfff1775dfb94e3d7a18ac05eb3fb1bd01a5
Polito Technologies <info@politotech.com>
2015-06-24 10:02:16 -0400

    Update salesforce-marketing.php
    Updated links

--- inc/salesforce-marketing.php
+++ inc/salesforce-marketing.php
@@ -6,12 +6,12 @@
         <div class="col-sm-6">
           <h3>Easy Campaign Member Status ›</h3>
           <p>Remembering to set the right campaign member statuses for every new campaign is a hassle. Leverage Easy Campaign Member Status to achieve consistent campaign member status values and get higher quality data and better reports. Set the right status values for your campaigns once and for all. </p>
-          <a href="#" class="btn btn-primary">get it on the appexchange ›</a>
+          <a href="http://104.236.242.180/full-wp/products/opportunity-gatekeeper/" class="btn btn-primary">get it on the appexchange ›</a>
         </div>
         <div class="col-sm-6">
           <h3>Opportunity Gatekeeper ›</h3>
           <p>Bad data affects everyone. And bad Opportunity data is the worst. It ruins your day and wastes time. Opportunity Gatekeeper saves you that time and makes it possible to get complete and accurate data on every Opportunity record.</p>
-          <a href="#" class="btn btn-primary">get it on the appexchange ›</a>
+          <a href="http://104.236.242.180/full-wp/products/easy-campaign-member-status/" class="btn btn-primary">get it on the appexchange ›</a>
         </div>
       </div>
     </div>
