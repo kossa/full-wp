adcd6cb02ec3e920286fedad6b44dd75a8bfa32b
Polito Technologies <info@politotech.com>
2015-07-08 14:17:00 -0400

    Update what-we-can-do.php
    update to pardot iframe

--- inc/what-we-can-do.php
+++ inc/what-we-can-do.php
@@ -1,7 +1,8 @@
 <section class="what-we-can-do">
   <div class="container">
     <h2><span>See what Full Circle can do for you</span></h2>
-    <?php echo do_shortcode( '[contact-form-7 id="4" title="What we can do"]' ); ?>
+    <!--<?php echo do_shortcode( '[contact-form-7 id="4" title="What we can do"]' ); ?>-->
+    <iframe src="http://go.pardot.com/l/69472/2015-06-22/gwm4w" width="100%" height="500" type="text/html" frameborder="0" allowTransparency="true" style="border: 0"></iframe>
     <div class="text-center">
       <img src="<?php echo bloginfo( 'url' ); ?>/wp-content/uploads/2015/05/img-05.png" alt="">
     </div>
