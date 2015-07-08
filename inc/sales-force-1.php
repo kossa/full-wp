9980b46a13228840ce0874e7461519ff31baa074
Polito Technologies <info@politotech.com>
2015-07-08 10:37:55 -0400

    Update sales-force-1.php

--- inc/sales-force-1.php
+++ inc/sales-force-1.php
@@ -4,7 +4,7 @@
     <div class="row">
       <div class="col-sm-9">
         <img src="<?php echo bloginfo( 'url' ); ?>/wp-content/uploads/2015/05/img-03.png" alt="">
-        <h2 class="white">Built Natively on the Salesforce1 Platform and available on the Appexchange </h2>
+        <h2 class="white">Built Natively on the Salesforce1 Platform and available on the AppExchange </h2>
         <div class="text-center"><a href="https://appexchange.salesforce.com/listingDetail?listingId=a0N3000000B4C4aEAF" class="btn btn-warning">Get it on the appexchange</a></div>
         <div class="visible-xs screen"><img src="img/img-24.png" alt=""></div>
       </div>
